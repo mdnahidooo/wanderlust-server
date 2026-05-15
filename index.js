f3884b6f2b9f0c5abd470b5d16690237a92e15f7
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-05-15 07:51:14 +0600

    some line must to do comment

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 });
 
 // for jwt
-const JWKS = createRemoteJWKSet(new URL("http://localhost:3000/api/auth/jwks"));
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
 
 const verifyToken = async (req, res, next) => {
     const authHeader = req?.headers.authorization;
@@ -59,7 +59,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         const db = client.db("wanderlust");
         const destinationCollection = db.collection("destinations");
@@ -136,7 +136,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
